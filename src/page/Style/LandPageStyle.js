909d662e1e2d0b4ca9d49c2cc402f72d590468b3
claudiocassimiro <days_surfing@live.com>
2022-02-12 13:09:12 -0300

    Estilos gerais para landpage

---
+++ src/page/Style/LandPageStyle.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const MainStyle = styled.main`
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  overflow-x: hidden;
+`;
+
+export default MainStyle;
