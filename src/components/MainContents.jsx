f3fbb8f2f0b39ee7bc0e3fd9485a3e604fc22f4f
claudiocassimiro <days_surfing@live.com>
2022-02-12 20:32:50 -0300

    Remove o form

--- src/components/MainContents.jsx
+++ src/components/MainContents.jsx
@@ -1,45 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ImageAppDemo from '../img/ImageSection1.svg';
+import ImageSection3 from '../img/ImageSection3.svg';
+import ImageSection4 from '../img/ImageSection4.svg';
+import ImageSection5 from '../img/ImageSection5.svg';
 import MainStyle, {
   SectionAppDemo,
-  ContainerTextAppDemo,
+  ArticleTextAppDemo,
   SectionNewsletter,
   ArticleNewsletter,
-  Form,
+  SectionPrices,
+  ArticlePaymantText,
+  SectionFramework,
+  SectionDailyDiare,
 } from './Style/MainContentsStyle';
-import sendDataToNewsletter from '../services/sendDataToNewsletter';
+import Form from './Form';
 
 const MainContents = () => {
-  const [enableButton, setEnableButton] = useState(true);
-  const [inputName, setInputName] = useState('');
-  const [inputEmail, setInputEmail] = useState('');
-
-  useEffect(() => {
-    if (inputName.length >= 3 && inputEmail.length >= 16) {
-      setEnableButton(false);
-    } else {
-      setEnableButton(true);
-    }
-  }, [inputName, inputEmail]);
-
-  const onSubmitForm = () => {
-    const name = inputName;
-    const email = inputEmail;
-
-    sendDataToNewsletter({ name, email });
-
-    setInputName('');
-    setInputEmail('');
-  };
 
   return (
     <MainStyle>
       <SectionAppDemo>
-        <ContainerTextAppDemo>
+        <ArticleTextAppDemo>
           <h2>Share your home,<br />nanny and costs</h2>
           <p>You have a fantastic home, a fantastic nanny and wouldn’t cutting your costs in half be, well, fantastic?! If only it was easy to connect with other parents to share your costs? Well now it is, with Hapu. <a href="#">Hapu means tribe</a> and it’s our foundational 3 tribal principles that empowers you to create and manage your own tribe. A tribe that together has the power to create new affordable solutions in childcare that work for you and your community.</p>
           <a href="#">Ready to get started?</a>
-        </ContainerTextAppDemo>
+        </ArticleTextAppDemo>
         <article>
           <img src={ImageAppDemo} alt="Image app demo" /> 
         </article>
@@ -50,31 +35,32 @@ const MainContents = () => {
           <h3>Are you a parent without a nanny and looking to share?</h3>
           <p>Leave us your name and email and we’ll update you as soon as a share becomes available in your area!</p>
         </ArticleNewsletter>
-        <Form onSubmit={(e) => e.preventDefault()}>
-          <input
-            type="text"
-            name="inputName"
-            value={inputName}
-            placeholder="Your name"
-            onChange={(e) => setInputName(e.target.value)}
-          />
-          <input
-            type="email"
-            placeholder="Your email"
-            name="inputEmail"
-            value={inputEmail}
-            onChange={(e) => setInputEmail(e.target.value)}
-          />
-          <button
-            type="submit"
-            disabled={enableButton}
-            onClick={onSubmitForm}
-          >
-            Send
-          </button>
-        </Form>
+        <Form />
       </SectionNewsletter>
       <hr />
+      <SectionPrices>
+        <article>
+          <img src={ImageSection3} alt="Prices" />
+        </article>
+        <ArticlePaymantText>
+          <h2>Shared payments made simple</h2>
+          <p>Sometimes it’s hard enough just sharing a restaurant bill. Try sharing that bill week in, week out and you might encounter more than a few snares. But not with Hapu. Simply set your rates and our automated payment system takes care of the rest. You need never talk money or who owes what.</p>
+          <a href="#">Read how Bridget’s share (without Hapu) ended over $15</a>
+        </ArticlePaymantText>
+      </SectionPrices>
+      <hr />
+      <SectionFramework>
+        <h2>A framework built for the long term</h2>
+        <p>Childcare is for the long term. And you need a framework you can count on that gives your share long term viability and success. That’s why we’ve defined Hapu around our three tribal principles; clearly defined process, transparency over money and equality of participation.</p>
+        <a href="#">Read how Hapu’s tribal background defines our app </a>
+        <img src={ImageSection4} alt="Nanny interface" />
+      </SectionFramework>
+      <hr />
+      <SectionDailyDiare>
+        <img src={ImageSection5} alt="Hupu daily diary" />
+        <h2>Coming soon: Nanny Share Daily Diary!</h2>
+        <p>With the Hapu daily diary your nanny will be able to update you and your sharers with photos and commentary of the day. You and sharers will receive notifications and you’ll be able to login to view the daily adventures fo your little ones. We can’t wait!</p>
+      </SectionDailyDiare>
     </MainStyle>
   );
 };
