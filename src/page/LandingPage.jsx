7bacb73b5819b83238d35b2bf5d359d4ca936a24
claudiocassimiro <days_surfing@live.com>
2022-02-12 13:07:50 -0300

    Implementa landingpage

---
+++ src/page/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import Rectangle from '../components/Rectangle';
+import MainStyle from './Style/LandPageStyle';
+
+const LandingPage = () => {
+
+  return (
+    <MainStyle>
+      <Header />
+      <Rectangle />
+    </MainStyle>  
+  )
+}
+
+export default LandingPage;
