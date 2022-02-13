40e0ddca13a117d9695058bdd07581dbc9ab8caa
claudiocassimiro <days_surfing@live.com>
2022-02-13 12:02:07 -0300

    Adiciona estilo responsivo ao componente

--- src/components/Styles/RectangleStyle.js
+++ src/components/Styles/RectangleStyle.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
 const MainStyle = styled.main`
+  @media (min-width: 360px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 216px;
+  }
   border: 1px solid #DFDFDF;
   background-color: #fff;
   display: flex;
@@ -12,6 +18,11 @@ const MainStyle = styled.main`
     margin-right: 20px;
   }
   & a {
+    @media (min-width: 360px) {
+      width: 212px;
+      height: 40px;
+      text-align: center;
+    }
     margin-right: 5px;
     color: #5E20A4;
     font-size: 16px;
@@ -19,6 +30,12 @@ const MainStyle = styled.main`
     line-height: 24px;
   }
   & p {
+    @media (min-width: 360px) {
+      width: 286px;
+      height: 20px;
+      font-size: 14px;
+      line-height: 20px;
+    }
     font-family: 'Helvetica Neue', sans-serif;
     font-size: 16px;
     font-weight: 500;
