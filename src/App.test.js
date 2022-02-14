eadb4ec84b98b9aace148ecc0d163897469dec21
claudiocassimiro <days_surfing@live.com>
2022-02-13 21:21:49 -0300

    Implementa tests para a seção hero da aplicação

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('test for application hero section', () => {
+  it('does the h1 element exist?', () => {
+    render(<App />);
+    const titleH1 = screen.getByRole('heading',
+      { level: 1, name: 'Easily create or join a local nanny share with Hapu' });
+      
+    expect(titleH1).toBeInTheDocument();
+  });
+
+  it('does the p element exist?', () => {
+    render(<App />);
+    const paragraph = screen.getByText(
+      'Hapu is Airbnb for nanny share. Share your home, nanny and costs and create new flexible, affordable solutions in childcare.'
+    );
+
+    expect(paragraph).toBeInTheDocument();
+  });
+})
